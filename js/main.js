be9b0cb91b3a5c290cfe55bca243fb59bc271092
John Thachil <jothachil@gmail.com>
2016-08-02 08:37:01 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,442 @@
+var colors = {
+   aqua:    '#7fdbff',
+   blue:    '#0074d9',
+   lime:    '#01ff70',
+   navy:    '#001f3f',
+   teal:    '#39cccc',
+   olive:   '#3d9970',
+   green:   '#2ecc40',
+   red:     '#ff4136',
+   maroon:  '#85144b',
+   orange:  '#ff851b',
+   purple:  '#b10dc9',
+   yellow:  '#ffdc00',
+   fuchsia: '#f012be',
+   gray:    '#aaaaaa',
+   white:   '#ffffff',
+   black:   '#111111',
+   silver:  '#dddddd'
+ };
+
+var bgarr = [
+  {
+ bgColor:colors.black,
+ textclr:colors.aqua
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.blue
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.lime
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.teal
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.olive
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.green
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.red
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.orange
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.purple
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.yellow
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.fuchsia
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.gray
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.white
+  },
+  {
+   bgColor:colors.black,
+   textclr:colors.silver
+  },
+  {
+   bgColor:colors.white,
+   textclr:colors.blue
+  },
+  {
+   bgColor:colors.white,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.white,
+   textclr:colors.olive
+  },
+  {
+   bgColor:colors.white,
+   textclr:colors.red
+  },
+  {
+   bgColor:colors.white,
+   textclr:colors.maroon
+  },
+  {
+   bgColor:colors.white,
+   textclr:colors.purple
+  },
+  {
+   bgColor:colors.white,
+   textclr:colors.fuchsia
+  },
+  {
+   bgColor:colors.white,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.silver,
+   textclr:colors.blue
+  },
+  {
+   bgColor:colors.silver,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.silver,
+   textclr:colors.maroon
+  },
+  {
+   bgColor:colors.silver,
+   textclr:colors.fucshia
+  },
+  {
+   bgColor:colors.silver,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.gray,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.gray,
+   textclr:colors.maroon
+  },
+  {
+   bgColor:colors.gray,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.fuchsia,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.fuchsia,
+   textclr:colors.white
+  },
+  {
+   bgColor:colors.fuchsia,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.yellow,
+   textclr:colors.purple
+  },
+  {
+   bgColor:colors.yellow,
+   textclr:colors.maroon
+  },
+  {
+   bgColor:colors.yellow,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.yellow,
+   textclr:colors.blue
+  },
+  {
+   bgColor:colors.yellow,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.purple,
+   textclr:colors.lime
+  },
+  {
+   bgColor:colors.purple,
+   textclr:colors.silver
+  },
+  {
+   bgColor:colors.purple,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.purple,
+   textclr:colors.white
+  },
+  {
+   bgColor:colors.purple,
+   textclr:colors.yellow
+  },
+  {
+   bgColor:colors.orange,
+   textclr:colors.maroon
+  },
+  {
+   bgColor:colors.orange,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.orange,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.maroon,
+   textclr:colors.lime
+  },
+  {
+   bgColor:colors.maroon,
+   textclr:colors.aqua
+  },
+  {
+   bgColor:colors.maroon,
+   textclr:colors.green
+  },
+  {
+   bgColor:colors.maroon,
+   textclr:colors.orange
+  },
+  {
+   bgColor:colors.maroon,
+   textclr:colors.yellow
+  },
+  {
+   bgColor:colors.maroon,
+   textclr:colors.gray
+  },
+  {
+   bgColor:colors.maroon,
+   textclr:colors.white
+  },
+  {
+   bgColor:colors.maroon,
+   textclr:colors.silver
+  },
+  {
+   bgColor:colors.red,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.red,
+   textclr:colors.white
+  },
+  {
+   bgColor:colors.red,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.aqua,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.aqua,
+   textclr:colors.maroon
+  },
+  {
+   bgColor:colors.aqua,
+   textclr:colors.fuchsia
+  },
+  {
+   bgColor:colors.aqua,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.blue,
+   textclr:colors.lime
+  },
+  {
+   bgColor:colors.blue,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.blue,
+   textclr:colors.yellow
+  },
+  {
+   bgColor:colors.blue,
+   textclr:colors.white
+  },
+  {
+   bgColor:colors.blue,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.blue,
+   textclr:colors.silver
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.blue
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.aqua
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.olive
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.green
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.lime
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.red
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.orange
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.yellow
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.fuchsia
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.gray
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.silver
+  },
+  {
+   bgColor:colors.navy,
+   textclr:colors.white
+  },
+  {
+   bgColor:colors.teal,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.teal,
+   textclr:colors.maroon
+  },
+  {
+   bgColor:colors.teal,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.lime,
+   textclr:colors.blue
+  },
+  {
+   bgColor:colors.lime,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.lime,
+   textclr:colors.maroon
+  },
+  {
+   bgColor:colors.lime,
+   textclr:colors.fuchsia
+  },
+  {
+   bgColor:colors.lime,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.olive,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.olive,
+   textclr:colors.white
+  },
+  {
+   bgColor:colors.olive,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.green,
+   textclr:colors.navy
+  },
+  {
+   bgColor:colors.green,
+   textclr:colors.black
+  },
+  {
+   bgColor:colors.green,
+   textclr:colors.maroon
+  }
+];
+
+function changeColor() {
+  $('#quote').fadeOut();
+  $('#title').fadeOut();
+  //Choose random color
+  var mycolor = bgarr[Math.floor(Math.random()*bgarr.length)];
+  //animate color **jquery color needed
+  console.log(mycolor);
+  $("html").animate({
+    backgroundColor:mycolor.bgColor,
+  }, 1000);
+  $(".bg-quote").animate({
+    color:mycolor.textclr
+  }, 1000);
+  $(".info").animate({
+    color:mycolor.textclr
+  }, 1000);
+
+
+  //ajax api call
+  $.ajax( {
+    url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+    beforeSend: function(){
+      $(  "#space" ).toggleClass( "space-bar" );
+    },
+    success: function(data) {
+      $(  "#space" ).toggleClass( "space-bar" );
+      $('#quote').html($(data[0].content).text()).fadeIn();
+      $('#title').html("- " + data[0].title).fadeIn();
+    },
+    cache: false
+  });
+}
+
+//modal trigger
+function displayModal() {
+  $('#modal1').toggle();
+}
+
+//keypress events
+$('body').keyup(function(e){
+  if(e.keyCode === 32){
+    changeColor();
+  }
+  if(e.keyCode === 73){
+    displayModal();
+  }
+});
